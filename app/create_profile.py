3d02073a4dc80fd6d4a1491a4584d0b95e37a26f
Elsie <156831523+oakgreybookshelf@users.noreply.github.com>
2026-03-16 13:30:33 -0400

    Update create_profile.py
    added WIP functions, still working on how to save profiles either to txt file or database

--- app/create_profile.py
+++ app/create_profile.py
@@ -8,3 +8,34 @@ class CreateProfile(BaseModel):
     username: int
     pronouns: string
     location: string
+
+
+class CreateProfile(BaseModel):
+    """
+    - creates user's profile
+    """
+    def __init__(self, name: str, age: int, pronouns: string | None, location: string):
+        self.name = string
+        self.age = int
+        self.pronouns = string
+        self.location = string
+
+    self.save_profile(name, age, pronouns, location)
+
+    def save_profile(n, a, p, l):
+        """
+        - saves user's profile to text file
+        """
+        file = open("profiles.txt", "a")
+        file.write(str(n, a, p, l))
+        file.close()
+        return
+
+    def delete_profile(n, a, p, l):
+        with open('profiles.txt', 'r') as file_record:
+        lines = file_record.readlines()
+        with open('profiles.txt', 'w') as file_writing:
+            if line != str(n, a, p, l):
+                file_writing.write(line)              
+        return    
+    
